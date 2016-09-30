ac3e22fcd0d43a69285f151619b0f75ef6fe9136
karensaroc <karensaroc@10.137.211.111>
2016-09-30 14:22:11 -0400

    Creating Depth-first search method.

--- VacuumRobot/src/model/SearchMethods.java
+++ VacuumRobot/src/model/SearchMethods.java
@@ -1,15 +1,43 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class SearchMethods {
 
 	private Grid grid;
-	
+	private Node startNode;
+	private Node goalNode;
+
 	public SearchMethods(Grid grid) {
 		this.grid = grid;
+		// startNode = new Node(null, )
+		// TODO generate start and goal node to suppy the searches.
 	}
 
-	public void DFS() {
-		
+	public Node DFS() {
+		Node result = null;
+		List<Node> closed = new ArrayList<>();
+		Stack<Node> fringe = new Stack<Node>();
+		fringe.push(startNode);
+		while (!fringe.isEmpty()) {
+			Node currentNode = fringe.pop();
+			if (currentNode.getState() == goalNode.getState()) {
+				result = currentNode;
+				break;
+			} else {
+				List<Node> children = getSuccessorDFS(currentNode);
+				closed.add(currentNode);
+				for (Node n : children) {
+					if (!closed.contains(n) && !fringe.contains(n)){
+						fringe.add(n);
+					}
+				}
+			}
+
+		}
+		return result;
 	}
 
 	public void BFS() {
@@ -19,9 +47,11 @@ public class SearchMethods {
 	public void AStar() {
 		// TODO Auto-generated method stub
 	}
-	
-	public void getSuccessorDFS(){
+
+	public List<Node> getSuccessorDFS(Node node) {
+		List<Node> children = new ArrayList<>();
 		
+		return children;
 	}
 
 }
